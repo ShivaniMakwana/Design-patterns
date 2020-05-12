0afd9056d623f6772695378263a29ef85332614b
Shivani Makwana <shivanimakwana256@gmail.com>
2020-05-12 15:52:25 +0530

    Add files via upload

---
+++ Strategy Pattern/src/DuckSimulator.java
@@ -0,0 +1,12 @@
+
+public class DuckSimulator {
+
+	public static void main(String[] args) {
+		Duck mallarduck = new MallarDuck();
+		mallarduck.performFly();
+		mallarduck.performQuack();
+		mallarduck.display();
+
+	}
+
+}
